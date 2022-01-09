ec816cf429c2ea19f057f8b3e595cf84693ba5ad
hsqoo <913650332abc@gmail.com>
2022-01-09 10:33:44 +0800

    Update subscribe.user.js

--- subscribe.user.js
+++ subscribe.user.js
@@ -6,6 +6,7 @@
 // @grant        GM_openInTab
 // @connect      hsqoo.github.io
 // @scriptUrl  https:/hsqoo.github.io/1.user.js
+// @scriptUrl  https:/hsqoo.github.io/2.user.js
 // @background
 // ==/UserSubscribe==
 const endTime = 1641398400000;  // 设定时间戳  *** 订阅脚本 ***
