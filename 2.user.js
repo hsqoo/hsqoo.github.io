4f0c842fa4fd0aaf1a0fb696b51cb0a9e166a16f
hsqoo <913650332abc@gmail.com>
2022-01-10 11:19:06 +0800

    Update 2.user.js

--- 2.user.js
+++ 2.user.js
@@ -1,29 +1,14 @@
 // ==UserScript==
-// @name         定时打开网页2
-// @description  订阅自动打开网页
+// @name         第二个脚本
+// @description  第二个脚本
 // @version      0.1.0
 // @author       You
 // @grant        GM_openInTab
 // @background
 // ==/UserScript==
-const endTime = 1641398400000;  // 设定时间戳  1月8号 最新一次
-const targetURL = 'https://taobao.com';
-let nowTime = '';  // 当前时间戳
-let timer = null;
-let nowDate = new Date().getTime();
 
 return new Promise((resolve, reject) => {
-    timer = setInterval(function() {
-        nowTime = new Date().getTime();
-        // 倒计时结束
-        console.log(nowTime, endTime)
-        if(nowTime >= endTime) {
-            GM_openInTab(targetURL, {active: true, insert: true, setParent: true});
-            console.log('倒计时结束-- 新打印');
-            
-            resolve('倒计时结束-- 新打印');
-            clearInterval(timer);
-        }
-    }, 1000);
+    
+    resolve('2022-01-10  第二个脚本');
     // Your code here...
 });
